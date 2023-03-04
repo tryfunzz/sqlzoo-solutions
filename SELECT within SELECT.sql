f375f37bf24724625069a155b0e4080fc5d7a8b0
XINMEI LUO <113983558+tryfunzz@users.noreply.github.com>
2023-03-04 12:27:23 +0800

    completed

--- SELECT within SELECT.sql
+++ SELECT within SELECT.sql
@@ -53,4 +53,59 @@ SELECT name, CONCAT(CAST(100*ROUND((population / (SELECT population
                                                   FROM world WHERE name ='Germany')), 2) AS INT), '%')
 FROM world
 WHERE continent = 'Europe'
-     
+
+--#6
+/*
+Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values)
+*/
+SELECT name
+FROM world
+WHERE GDP > ALL(SELECT GDP 
+                FROM world
+                WHERE continent = 'Europe' AND GDP>0)
+                
+--#7
+/*
+Find the largest country (by area) in each continent, show the continent, the name and the area
+*/
+SELECT continent, name, area 
+FROM world x
+WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area >0)
+          
+                
+--#8
+/*
+List each continent and the name of the country that comes first alphabetically.
+*/          
+SELECT continent, name
+FROM world x
+WHERE name = (SELECT TOP 1 name FROM world y
+              WHERE y.continent=x.continent
+              ORDER BY name ASC)
+
+--#9
+/*
+Find the continents where all countries have a population <= 25000000. 
+Then find the names of the countries associated with these continents. 
+Show name, continent and population.
+*/
+SELECT name, continent, population
+FROM world x
+WHERE 25000000 > ALL(SELECT population 
+                     FROM world y 
+                     WHERE y.continent = x.continent)
+
+--#10
+/*
+Some countries have populations more than three times that of all of their neighbours (in the same continent). Give the countries and continents.
+*/
+SELECT name, continent
+FROM world x
+WHERE  population  > ALL (SELECT 3*population 
+                          FROM world y 
+                          WHERE y.continent = x.continent 
+                           AND y.name != x.name
+                           AND population > 0)
